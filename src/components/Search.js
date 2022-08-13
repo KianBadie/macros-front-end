cf1a7d7d9a59964aa0796b82425462e74ed9ac32
Kian Badie <kianbadie@gmail.com>
2022-08-12 22:24:07 -0700

    Add barebones Search component.

---
+++ src/components/Search.js
@@ -0,0 +1,12 @@
+function Search(props) {
+    return (
+        <div>
+            <label htmlFor='food-search-input'>
+                Search
+            </label>
+            <input id='food-search-input' type='search'/>
+        </div>
+    );
+}
+
+export default Search;
