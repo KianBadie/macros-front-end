b80a3bfa4cdecfcf25b5a91938771ba88a90cd88
Kian Badie <kianbadie@gmail.com>
2022-08-28 13:47:05 -0700

    Add initial styles to Result component.

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,14 +1,18 @@
+import './Result.css';
+
 function Result(props) {
 
     const food = props.food;
     const energyNutrient = food.foodNutrients.find(el => el.nutrientId === 1008);
 
     return (
-        <li>
-            <h3>{food.description}</h3>
-            <p>{food.brandName}</p>
-            <p>{energyNutrient.value}{energyNutrient.unitName} / 100{food.servingSizeUnit}</p>
-            <button onClick={() => props.addIngredient(food)}>Add</button>
+        <li className='search__result result'>
+            <div className='result__title-container'>
+                <h3 className='result__title'>{food.description}</h3>
+                <p className='result__brand'>{food.brandName}</p>
+            </div>
+            <p className='result__calories'>{energyNutrient.value}{energyNutrient.unitName} / 100{food.servingSizeUnit}</p>
+            <button className='result__btn' onClick={() => props.addIngredient(food)}>Add</button>
         </li>
     );
     
