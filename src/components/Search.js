97753fcbefc4d956b0693e0b1895eba6a111bc75
Kian Badie <kianbadie@gmail.com>
2022-09-01 12:18:14 -0700

    Change Search to require submit for search.

--- src/components/Search.js
+++ src/components/Search.js
@@ -9,6 +9,19 @@ function Search(props) {
     const [query, setQuery] = useState('');
     const [results, setResults] = useState([]);
 
+    function handleSubmit(e) {
+        const fetchData = async () => {
+            const res = await fetch(`http://${window.location.hostname}:8000/search/${query}`);
+            const data = await res.json();
+            const results = data.foods.filter( (food) => food.foodNutrients.some((nutrient) => nutrient.nutrientId == 1008) );
+            setResults(results);
+        };
+
+        e.preventDefault();
+
+        if(query) fetchData();
+    }
+
     function handleChange(e) {
         setQuery(e.target.value);
     }
@@ -18,21 +31,6 @@ function Search(props) {
         setResults([]);
         props.addIngredient(newFood);
     }
-
-    useEffect(() => {
-        const fetchData = async () => {
-            const res = await fetch(`http://${window.location.hostname}:8000/search/${query}`);
-            const data = await res.json();
-            const results = data.foods.filter( (food) => food.foodNutrients.some((nutrient) => nutrient.nutrientId == 1008) );
-            setResults(results);
-        };
-
-        let fetchTimeout;
-
-        if(query) fetchTimeout = setTimeout(fetchData, 1000);
-
-        return () => clearTimeout(fetchTimeout);
-    }, [query]);
 
     const resultList = results.map(result => (
         <Result
@@ -46,13 +44,16 @@ function Search(props) {
         <div className={styles.search}>
             <SectionTitle title='Search' />
             <div className={`${styles['search-container']} ${resultList.length > 0 ? styles['search-container-results'] : ''}`}>
-                <input 
-                    type='search'
-                    value={query}
-                    onChange={handleChange}
-                    className={styles.bar}
-                    placeholder='Jasmine Rice...'
-                />
+                <form onSubmit={handleSubmit} className={styles.form}>
+                    <input 
+                        type='search'
+                        value={query}
+                        onChange={handleChange}
+                        className={styles.input}
+                        placeholder='Jasmine Rice...'
+                    />
+                    <button type='submit' className={styles.submit}>Search</button>
+                </form>
                 <ul className={styles['result-list']}>
                     {resultList}
                 </ul>
