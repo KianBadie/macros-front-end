ba809c9921784f6c032afc847ff9cfc193e6f5f4
Kian Badie <kianbadie@gmail.com>
2022-09-05 11:38:49 -0700

    Add drop shadow to AnalyticsChart.

--- src/components/AnalyticsChart.js
+++ src/components/AnalyticsChart.js
@@ -7,6 +7,9 @@ function AnalyticsChart(props) {
 
     return (
         <svg viewBox='0 0 400 400'>
+            <filter id='drop-shadow'>
+                <feDropShadow dx='0' dy='0' stdDeviation='2' floodColor='#000' floodOpacity='0.4'/>
+            </filter>
             <VictoryPie
                 standalone={false}
                 data={data}
@@ -18,7 +21,10 @@ function AnalyticsChart(props) {
                 innerRadius={96}
                 padAngle={2}
                 animate={{ duration: 500 }}
-                style={{ labels: { fontFamily: 'Roboto', fontWeight: 300 } }}
+                style={{ 
+                    labels: { fontFamily: 'Roboto', fontWeight: 300 }, 
+                    data: { filter: 'url(#drop-shadow)' }
+                }}
             />
             <VictoryLabel
                 textAnchor='middle'
