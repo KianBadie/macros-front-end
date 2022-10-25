a139d067d35df2752325cd0c0b14b79fa4e21ebd
Kian Badie <kianbadie@gmail.com>
2022-10-25 09:08:22 -0700

    Remove server logic to handle root routes as that is handeled in static folder.

--- server/index.js
+++ server/index.js
@@ -8,11 +8,6 @@ const PORT = process.env.PORT || 8000;
 const app = express();
 
 app.use(express.static(path.resolve(__dirname, '../build')));
-
 app.use('/api', api);
-
-app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../build', 'index.html'));
-});
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
